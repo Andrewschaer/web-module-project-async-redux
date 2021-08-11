bc9af8062446703fdfb9d0ade1dd5716c3cfd129
Andrewschaer <andrewschaer@github.com>
2021-08-11 17:32:29 -0400

    React app default files deleted... ready to begin building!

---
+++ app/src/Components/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>WHATS UP THIS IS MY APP</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
