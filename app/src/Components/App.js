7009b59557f8e9ca8ac01331d4ac2f6d25ab0759
Andrewschaer <andrewschaer@github.com>
2021-08-11 18:46:38 -0400

    added imports to index.js, added in the foundation of a reducer, and built out skeleton html structure to render in app.js

--- app/src/Components/App.js
+++ app/src/Components/App.js
@@ -1,11 +1,26 @@
-
+import React from 'react';
+import '../Styling/index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          <h1>WHATS UP THIS IS MY APP</h1>
+    <div className='App'>
+      <header className='Header'>
+          <h1>TRUE/FALSE TRIVIA</h1>
+          <nav>
+            <button>Change Category</button>
+            <button>Trivia History</button>
+          </nav>
       </header>
+      <div className='UserRecordDetails'>
+        <div><p>Your Record: 0-0</p></div>
+        <div><p>Active Streak: 0</p></div>
+      </div>
+      <div>
+        <h2>MAIN SECTION COMPONENT</h2>
+      </div>
+      <footer>
+        <p>Thank you for playing!  This website uses the triviaAPI from OpenTriviaDatabase (https://opentdb.com/)</p>
+      </footer>
     </div>
   );
 }
